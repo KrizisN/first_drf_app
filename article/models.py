4b4eda7f276197bed4e44988a68bf0b59c1885ed
NikitaKr <nikita.fyvfyv@gmail.com>
2020-12-07 17:25:26 +0200

    Add view, article/urls, and modified models

--- article/models.py
+++ article/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from django.db import models
 
 
 class Author(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.name
 
 
 class Article(models.Model):
@@ -12,3 +14,6 @@ class Article(models.Model):
     description = models.TextField()
     body = models.TextField()
     author = models.ForeignKey('Author', related_name='articles', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
